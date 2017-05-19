6a101f091fb7acbf0354f7a0eae12e7cd06c238d
songhee <20150261sh@gmail.com>
2017-05-19 15:08:05 +0900

    map 안나오게

---
+++ p.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#define N 30
+int main(void)
+{
+    int i=0,j=0,k;
+
+    char map[5][N][N]={0};
+
+    FILE *ifp;
+    char c;
+    ifp = fopen("C:\\cygwin64\\home\\이송희\\cpro\\map.txt","r");
+
+    if(ifp==NULL)printf("no");
+    for(k=0;k<5;k++)
+    {
+        for(i=0;i<N;i++)
+        {
+            for(j=0;j<N;j++)
+            {
+                fscanf(ifp,"%c",&map[k][i][j]);
+                if(map[k][i][0]=='m')
+                    break;	
+                if(map[k][i][j]=='\n')
+                    break;
+            }
+            if(map[k][i][0]=='m')
+            {
+                map[k][i][0]='\0';
+                break;	
+            }
+        }
+        fgetc(ifp);
+        fgetc(ifp);
+        fgetc(ifp);
+    }	
+    for(k = 0; k < 5; k++) {
+        for(i=0;i<N;i++)
+            for(j=0;j<N;j++)
+                printf("%c",map[k][i][j]);
+        printf("\n");
+    }
+
+    fclose(ifp);
+    return 0;
+}
+
+
